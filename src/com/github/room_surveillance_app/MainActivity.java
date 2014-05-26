78e453a95a084f0f33a15880d4795b6694891981
Your Name <you@example.com>
2014-05-26 13:55:02 +0200

    added camera activity

--- src/com/github/room_surveillance_app/MainActivity.java
+++ src/com/github/room_surveillance_app/MainActivity.java
@@ -2,14 +2,19 @@ package com.github.room_surveillance_app;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class MainActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
     }
 
 
@@ -56,8 +62,23 @@ public class MainActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+//            final Button camera_btn = (Button) rootView.findViewById(R.id.btn_camera);
+//            camera_btn.setOnClickListener(new View.OnClickListener() {
+//                public void onClick(View v) {
+//                    Log.d(TAG, "Camera Button pressed.");
+//                    camera();
+//                }
+//            });
             return rootView;
         }
     }
 
+    public void startCamera(View view) {
+    	Intent intent = new Intent(this, CameraActivity.class);
+//    	EditText editText = (EditText) findViewById(R.id.edit_message);
+//    	String message = editText.getText().toString();
+//    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
+    
 }
