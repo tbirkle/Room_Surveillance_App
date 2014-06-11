b571f57d10b65c73a4c4402b6f59136121116a29
Your Name <you@example.com>
2014-06-11 13:17:22 +0200

    imagesactivity added

--- Room_Surveillance_App/src/com/github/Room_Surveillance_App/ImagesActivity.java
+++ Room_Surveillance_App/src/com/github/Room_Surveillance_App/ImagesActivity.java
@@ -1,23 +1,34 @@
 package com.github.Room_Surveillance_App;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends Activity {
+public class ImagesActivity extends Activity {
+
+	private static final String LOG_TAG = "ImagesActivity";
+	private static final String TAG = "ImagesActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_images);
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
@@ -29,7 +40,7 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.images, menu);
 		return true;
 	}
 
@@ -56,15 +67,17 @@ public class MainActivity extends Activity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+			View rootView = inflater.inflate(R.layout.fragment_images,
+					container, false);
 			return rootView;
 		}
 	}
-
-	public void startImagesActivity(View view) {
-		Intent intent = new Intent(this, ImagesActivity.class);
-    	startActivity(intent);
+	
+	public void startDownload(View view)
+	{
+		//download("http://citiesofmigration.ca/wp-content/uploads/2011/02/test.jpg");
+		Log.d(TAG, "downloaded");
 	}
+	
 	
 }
